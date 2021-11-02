4256e0a37b129c6aef45d8523d142c3c39faf557
Miles-Garnsey <miles.garnsey@datastax.com>
2021-11-02 18:09:27 +1100

    Add new certs and make them take 5 years to expire.

--- manifests/demoCA/generate-certs.sh
+++ manifests/demoCA/generate-certs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 openssl genrsa -out manifests/demoCA/tls.key 4096
-openssl req -new -x509 -key manifests/demoCA/tls.key -out manifests/demoCA/tls.crt -subj "/C=AU/ST=NSW/L=Sydney/O=Global Security/OU=IT Department/CN=example.com"
+openssl req -new -x509 -days 1825 -key manifests/demoCA/tls.key -out manifests/demoCA/tls.crt -subj "/C=AU/ST=NSW/L=Sydney/O=Global Security/OU=IT Department/CN=example.com"
+# If on mac find openssl under `/usr/local/Cellar/openssl...` and use it instead of the default openssl. 
